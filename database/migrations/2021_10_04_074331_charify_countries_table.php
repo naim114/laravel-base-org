3d9fe131728aa33800f2f336eac2229092828826
naim114 <naim.muhammad99@gmail.com>
2022-09-05 21:29:34 +0800

    make it better

--- database/migrations/2021_10_04_074331_charify_countries_table.php
+++ database/migrations/2021_10_04_074331_charify_countries_table.php
@@ -13,11 +13,11 @@ class CharifyCountriesTable extends Migration
     public function up()
     {
         Schema::table(\Config::get('countries.table_name'), function ($table) {
-            DB::statement("ALTER TABLE " . DB::getTablePrefix() . \Config::get('countries.table_name') . " ALTER COLUMN country_code TYPE CHAR(3) NOT NULL DEFAULT ''");
-            DB::statement("ALTER TABLE " . DB::getTablePrefix() . \Config::get('countries.table_name') . " ALTER COLUMN iso_3166_2 TYPE CHAR(2) NOT NULL DEFAULT ''");
-            DB::statement("ALTER TABLE " . DB::getTablePrefix() . \Config::get('countries.table_name') . " ALTER COLUMN iso_3166_3 TYPE CHAR(3) NOT NULL DEFAULT ''");
-            DB::statement("ALTER TABLE " . DB::getTablePrefix() . \Config::get('countries.table_name') . " ALTER COLUMN region_code TYPE CHAR(3) NOT NULL DEFAULT ''");
-            DB::statement("ALTER TABLE " . DB::getTablePrefix() . \Config::get('countries.table_name') . " ALTER COLUMN sub_region_code TYPE CHAR(3) NOT NULL DEFAULT ''");
+            DB::statement("ALTER TABLE " . DB::getTablePrefix() . \Config::get('countries.table_name') . " ALTER COLUMN country_code TYPE CHAR(3) SET NOT NULL DEFAULT ''");
+            DB::statement("ALTER TABLE " . DB::getTablePrefix() . \Config::get('countries.table_name') . " ALTER COLUMN iso_3166_2 TYPE CHAR(2) SET NOT NULL DEFAULT ''");
+            DB::statement("ALTER TABLE " . DB::getTablePrefix() . \Config::get('countries.table_name') . " ALTER COLUMN iso_3166_3 TYPE CHAR(3) SET NOT NULL DEFAULT ''");
+            DB::statement("ALTER TABLE " . DB::getTablePrefix() . \Config::get('countries.table_name') . " ALTER COLUMN region_code TYPE CHAR(3) SET NOT NULL DEFAULT ''");
+            DB::statement("ALTER TABLE " . DB::getTablePrefix() . \Config::get('countries.table_name') . " ALTER COLUMN sub_region_code TYPE CHAR(3) SET NOT NULL DEFAULT ''");
         });
     }
     /**
@@ -28,11 +28,11 @@ class CharifyCountriesTable extends Migration
     public function down()
     {
         Schema::table(\Config::get('countries.table_name'), function ($table) {
-            DB::statement("ALTER TABLE " . DB::getTablePrefix() . \Config::get('countries.table_name') . " ALTER COLUMN country_code TYPE VARCHAR(3) NOT NULL DEFAULT ''");
-            DB::statement("ALTER TABLE " . DB::getTablePrefix() . \Config::get('countries.table_name') . " ALTER COLUMN iso_3166_2 TYPE VARCHAR(2) NOT NULL DEFAULT ''");
-            DB::statement("ALTER TABLE " . DB::getTablePrefix() . \Config::get('countries.table_name') . " ALTER COLUMN iso_3166_3 TYPE VARCHAR(3) NOT NULL DEFAULT ''");
-            DB::statement("ALTER TABLE " . DB::getTablePrefix() . \Config::get('countries.table_name') . " ALTER COLUMN region_code TYPE VARCHAR(3) NOT NULL DEFAULT ''");
-            DB::statement("ALTER TABLE " . DB::getTablePrefix() . \Config::get('countries.table_name') . " ALTER COLUMN sub_region_code TYPE VARCHAR(3) NOT NULL DEFAULT ''");
+            DB::statement("ALTER TABLE " . DB::getTablePrefix() . \Config::get('countries.table_name') . " ALTER COLUMN country_code TYPE VARCHAR(3) SET NOT NULL DEFAULT ''");
+            DB::statement("ALTER TABLE " . DB::getTablePrefix() . \Config::get('countries.table_name') . " ALTER COLUMN iso_3166_2 TYPE VARCHAR(2) SET NOT NULL DEFAULT ''");
+            DB::statement("ALTER TABLE " . DB::getTablePrefix() . \Config::get('countries.table_name') . " ALTER COLUMN iso_3166_3 TYPE VARCHAR(3) SET NOT NULL DEFAULT ''");
+            DB::statement("ALTER TABLE " . DB::getTablePrefix() . \Config::get('countries.table_name') . " ALTER COLUMN region_code TYPE VARCHAR(3) SET NOT NULL DEFAULT ''");
+            DB::statement("ALTER TABLE " . DB::getTablePrefix() . \Config::get('countries.table_name') . " ALTER COLUMN sub_region_code TYPE VARCHAR(3) SET NOT NULL DEFAULT ''");
         });
     }
 }
